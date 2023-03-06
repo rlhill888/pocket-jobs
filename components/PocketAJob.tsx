62dca41693fe02a5547269773b3f2f483c437e00
rlhill888 <rlhill888@gmail.com>
2023-03-06 02:50:12 -0500

    updated pocked a job creation

--- components/PocketAJob.tsx
+++ components/PocketAJob.tsx
@@ -1,7 +1,12 @@
 import { defaultColumns } from '@/lib/database';
-import { TextField } from '@mui/material';
+import { Button, IconButton, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import Checkbox from '@mui/material/Checkbox';
+import AddIcon from '@mui/icons-material/Add';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import CancelTwoToneIcon from '@mui/icons-material/CancelTwoTone';
 import "@/styles/components.css/PocketAJob.css"
 
 interface PocketAJobProps
@@ -9,11 +14,19 @@ interface PocketAJobProps
     jobBoard: any;
 }
 
+interface aStep{
+    name: string;
+    dueDate?: string;
+    stepDescription?: string | null;
+    id: Number;
+}
+
 export default function PocketAJob({
     jobBoard
 }:PocketAJobProps){
 const extraColumns = JSON.parse(jobBoard.extraJobColumns)
 const [values, setValues]= useState(declareInitialValuesState())
+const [steps, setSteps]: [aStep[], Function]= useState([])
 function declareInitialValuesState(){
     const allValues = [...defaultColumns, ...extraColumns]
 
@@ -93,7 +106,7 @@ function mapOutInputs(){
                 if(column.columnType === 'text'){
                     return (
                     <div  key={`pocketed job input ${index} ${column.columnName}`}>
-                        <TextField sx={{width: "100%"}} className='pocketedJobInputDiv' label={column.columnName} placeholder={column.columnName}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
+                        <TextField color={index % 2 ? 'primary' : 'secondary'} variant='standard' sx={{width: "100%"}} className='pocketedJobInputDiv' label={column.columnName} placeholder={column.columnName}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
                             let copyArray = [...previous]
                             copyArray[index].value = e.target.value
                             return copyArray
@@ -105,7 +118,7 @@ function mapOutInputs(){
                     return(
                     <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnName}`}>
 
-                        <TextField sx={{width: "100%"}} label={column.columnName} placeholder={`${column.columnName} Number`} type='number'  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
+                        <TextField color={index % 2 ? 'primary' : 'secondary'} variant='standard' sx={{width: "100%"}} label={column.columnName} placeholder={`${column.columnName} Number`} type='number'  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
                             let copyArray = [...previous]
                             copyArray[index].value = e.target.value
                             return copyArray
@@ -115,9 +128,9 @@ function mapOutInputs(){
                 }
                 if(column.columnType === 'checkbox'){
                     return(
-                       <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnName}`}>
+                       <div className='pocketedJobInputDiv2' key={`pocketed job input ${index} ${column.columnName}`}>
                         <h3>{column.columnName}</h3>
-                        <Checkbox  checked={values[index].value as boolean} onChange={(e)=> setValues((previous: any)=>{
+                        <Checkbox  color={index % 2 ? 'primary' : 'secondary'} checked={values[index].value as boolean} onChange={(e)=> setValues((previous: any)=>{
                         let copyArray = [...previous]
                         copyArray[index].value = e.target.checked
                         return copyArray
@@ -127,9 +140,9 @@ function mapOutInputs(){
                 }
                 if(column.columnType === 'date'){
                     return( 
-                    <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnName}`}>
+                    <div className='pocketedJobInputDivDate' key={`pocketed job input ${index} ${column.columnName}`}>
                         <h3>{column.columnName}</h3>
-                        <TextField sx={{width: "100%"}}    type='date'  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
+                        <TextField color={index % 2 ? 'primary' : 'secondary'} label={""} variant='standard' sx={{width: "100%"}}    type='date'  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
                             let copyArray = [...previous]
                             copyArray[index].value = e.target.value
                             return copyArray
@@ -140,7 +153,7 @@ function mapOutInputs(){
                 if(column.columnType === 'link'){
                     return( 
                     <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnName}`}>
-                        <TextField sx={{width: "100%"}} label={column.columnName} placeholder={column.columnType}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
+                        <TextField color={index % 2 ? 'primary' : 'secondary'} variant='standard' sx={{width: "100%"}} label={column.columnName} placeholder={column.columnType}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
                             let copyArray = [...previous]
                             copyArray[index].value = e.target.value
                             return copyArray
@@ -154,7 +167,7 @@ function mapOutInputs(){
                 if(column.columnType === 'phone number'){
                     return(
                         <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnType}`}>
-                        <TextField sx={{width: "100%"}} type='tel' label={column.columnName} placeholder={column.columnName}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
+                        <TextField color={index % 2 ? 'primary' : 'secondary'} variant='standard' sx={{width: "100%"}} type='tel' label={column.columnName} placeholder={column.columnName}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
                         let copyArray = [...previous]
                         copyArray[index].value = e.target.value
                         return copyArray
@@ -165,7 +178,7 @@ function mapOutInputs(){
                 if(column.columnType === 'email'){
                     return( 
                     <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnType}`}>
-                    <TextField sx={{width: "100%"}} type='email' label={column.columnName} placeholder={column.columnName}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
+                    <TextField color={index % 2 ? 'primary' : 'secondary'} variant='standard' sx={{width: "100%"}} type='email' label={column.columnName} placeholder={column.columnName}  value={values[index].value} onChange={(e)=> setValues((previous: any)=>{
                         let copyArray = [...previous]
                         copyArray[index].value = e.target.value
                         return copyArray
@@ -175,7 +188,7 @@ function mapOutInputs(){
                 }
                 if(column.columnType === 'color'){
                     return( 
-                    <div className='pocketedJobInputDiv' key={`pocketed job input ${index} ${column.columnName}`}>
+                    <div className='pocketedJobInputDiv2' key={`pocketed job input ${index} ${column.columnName}`}>
                     <h3>{column.columnName}</h3>
                     <input type='color' value={values[index].value as string}   onChange={(e)=> setValues((previous: any)=>{
                         let copyArray = [...previous]
@@ -193,6 +206,8 @@ function mapOutInputs(){
 
 
 
+
+
     return (
         <div>
             <h2>Pocket a Job</h2>
@@ -201,7 +216,119 @@ function mapOutInputs(){
             </div>
             <hr className='pocketAJobH'/>
             <div>
+            <div className='jobStepsCreatePocketedJob'>
+                <h3>Job Steps</h3>
+                <Button
+                onClick={()=>{
+                    setSteps((previous: aStep[])=>{
+                        let copyArray = [...previous]
+                        copyArray.push({
+                            name: "",
+                            stepDescription: null,
+                            dueDate: "",
+                            id: copyArray.length + 1
+                        })
+                        return copyArray
+                    })
+                }}
+                > <AddIcon /> Add A Step</Button>
+            </div>
+            <div className='createPocketJobStepContent'>
 
+                <Stepper>
+                {steps.map((step: aStep, index)=>{
+                return(
+                    <Step key={` create pocked job name step ${step.id}`}>
+                        <StepLabel>
+                            <TextField  value={step.name} onChange={(e)=>{
+                                setSteps((previous: aStep[])=>{
+                                    let copyArray = [...previous]
+                                    copyArray[index].name = e.target.value
+                                    return copyArray
+                                })
+                            }} variant='standard' color={index % 2 ? 'primary' : 'secondary'}></TextField>
+                        </StepLabel>
+                    </Step>
+                )
+                 })}
+
+                </Stepper>
+                <div className='stepDescriptionsDiv'>
+                    {steps.map((individualStep: aStep, index)=>{
+                        const step = individualStep
+                        console.log(step)
+                        if(typeof(step.stepDescription) === 'string'){
+                            return(
+                            <div style={{marginTop: "30px"}} key={`step description create pocketed job  ${step.id}`}> 
+
+                                <div className='displayFlexRowHeaderJustifyContentSpaceBetween'>
+                                    <div className='displayFlexRowHeader'>
+                                        <IconButton 
+                                        onClick={(e)=>{
+                                            setSteps((previous: aStep[])=>{
+                                                let copyArray= [...previous]
+                                                copyArray.splice(index, 1)
+                                                return copyArray
+                                            })
+                                        }} 
+                                        sx={{color: 'red'}}>
+                                            <CancelTwoToneIcon />
+                                        </IconButton>
+                                        <h3>{index +1}. {step.name} description</h3>
+                                    </div>
+                                    
+                                    <Button
+                                    onClick={()=>{
+                                        setSteps((previous: aStep[])=>{
+                                            let copyArray = [...previous]
+                                            copyArray[index].stepDescription = null
+                                            return copyArray
+                                        })
+                                    }}
+                                    > Remove Step {index +1} Description</Button>
+                                </div>
+                                
+                                <TextField value={step.stepDescription} onChange={(e: any)=> setSteps((previous: aStep[])=>{
+                                    let copyArray = [...previous]
+                                    copyArray[index].stepDescription = e.target.value 
+                                    return copyArray
+                                })} sx={{width: "100%"}}></TextField>
+
+                            </div>
+                            )
+                        }else{
+                            return(
+                                <div className='descriptionButtonAndRemoveIconDivCreatePocketedJob' key={`step description create pocketed job  ${step.id}`}>
+                                    <IconButton 
+                                    onClick={(e)=>{
+                                        setSteps((previous: aStep[])=>{
+                                            let copyArray= [...previous]
+                                            copyArray.splice(index, 1)
+                                            return copyArray
+                                        })
+                                    }} 
+                                    sx={{color: 'red'}}>
+                                        <CancelTwoToneIcon />
+                                    </IconButton>
+                                    <Button sx={{width: "auto"}}
+                                    onClick={()=>{
+                                        setSteps((previous: aStep[])=>{
+                                            let copyArray = [...previous]
+                                            copyArray[index].stepDescription= ""
+                                            return copyArray
+                                        })
+                                    }}
+                                    >Add description for step {index +1}</Button>
+                                </div>
+                            )
+                            
+                        }
+                    })}
+                </div>
+
+            
+            </div>
+                
             </div>
             
         </div>
